419bb7bc96b90106e306d61706d988650d2ceb98
Derrick Thornton <derrickguitarmaster@gmail.com>
2019-05-14 23:25:32 +0100

    The hex code of the colors show.

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-var colorOne, colorTwo, input;
+var colorOne, colorTwo, input, backgroundColors, backgroundText;
 
 colorOne = document.querySelector('.colorOne');
 colorTwo = document.querySelector('.colorTwo');
 input = document.querySelector('input');
+backgroundColors = document.querySelector('h3');
+backgroundText = document.createTextNode(`${colorOne.value} & ${colorTwo.value}`)
+backgroundColors.appendChild(backgroundText);
 
 colorOne.addEventListener("input", changeColor);
-
 colorTwo.addEventListener("input", changeColor);
 
 function changeColor() {
 	document.body.style = `background: linear-gradient(to right, ${colorOne.value}, ${colorTwo.value})`;
+	backgroundColors.removeChild(backgroundText);
+	backgroundColors.appendChild(checkColor());
+}
+function checkColor() {
+	backgroundText = document.createTextNode(`${colorOne.value} & ${colorTwo.value}`)
+	return backgroundText;
 }
