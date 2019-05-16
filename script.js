90bcbde82c3a4b25ac73e2c3236e9bc8765a8615
Derrick Thornton <derrickguitarmaster@gmail.com>
2019-05-16 18:26:37 +0100

    Updated to ES6 - arrow functions

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var colorOne, colorTwo, input, backgroundColorInfo, backgroundTextNode;
+let colorOne, colorTwo, input, backgroundColorInfo, backgroundTextNode;
 
 colorOne = document.querySelector('.colorOne');
 colorTwo = document.querySelector('.colorTwo');
@@ -7,15 +7,15 @@ backgroundColorInfo = document.querySelector('h3');
 backgroundTextNode = document.createTextNode(`${colorOne.value} & ${colorTwo.value}`)
 backgroundColorInfo.appendChild(backgroundTextNode);
 
-colorOne.addEventListener("input", changeColor);
-colorTwo.addEventListener("input", changeColor);
-
-function changeColor() {
+let changeColor = () => {
 	document.body.style = `background: linear-gradient(to right, ${colorOne.value}, ${colorTwo.value})`;
 	backgroundColorInfo.removeChild(backgroundTextNode);
 	backgroundColorInfo.appendChild(checkColor());
 }
-function checkColor() {
+let checkColor = () => {
 	backgroundTextNode = document.createTextNode(`${colorOne.value} & ${colorTwo.value}`)
 	return backgroundTextNode;
 }
+
+colorOne.addEventListener("input", changeColor);
+colorTwo.addEventListener("input", changeColor);
